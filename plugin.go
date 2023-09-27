bb5bc6ef354bcd002d6cdeb04be68a056306e710
Alex Eagle <alex@aspect.dev>
2023-09-26 19:32:48 -0700

    feat: add a lint command to bazel

---
+++ plugin.go
@@ -0,0 +1,148 @@
+// This is a plugin for Aspect CLI to add a 'lint' command
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	goplugin "github.com/hashicorp/go-plugin"
+
+	"aspect.build/cli/bazel/command_line"
+	"aspect.build/cli/pkg/aspecterrors"
+	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/ioutils"
+	"aspect.build/cli/pkg/plugin/sdk/v1alpha4/config"
+	aspectplugin "aspect.build/cli/pkg/plugin/sdk/v1alpha4/plugin"
+)
+
+var Log = log.Default()
+
+var QUIET_BZL_ARGS = []string{"--output_filter", "DONT_MATCH_ANYTHING", "--noshow_progress", "--show_loading_progress"}
+
+// main starts up the plugin as a child process of the CLI and connects the gRPC communication.
+func main() {
+	goplugin.Serve(config.NewConfigFor(&LintPlugin{}))
+}
+
+// LintPlugin declares the fields on an instance of the plugin.
+type LintPlugin struct {
+	// Base gives default implementations of the plugin methods, so implementing them below is optional.
+	// See the definition of aspectplugin.Base for more methods that can be implemented by the plugin.
+	aspectplugin.Base
+	// This plugin will store some state from the Build Events for use at the end of the build.
+	command_line.CommandLine
+}
+
+// CustomCommands contributes a new 'lint' command alongside the built-in ones like 'build' and 'test'.
+func (plugin *LintPlugin) CustomCommands() ([]*aspectplugin.Command, error) {
+	// TODO: streams within the aspectplugin.Command are no longer std out/err and become logging.
+	streams := ioutils.Streams{
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	return []*aspectplugin.Command{
+		aspectplugin.NewCommand(
+			"lint",
+			"Run configured linters over the dependency graph.",
+			"Run linters and collect the reports they produce. TODO: more usage docs",
+			func(ctx context.Context, args []string, bazelStartupArgs []string) error {
+				// Build with the linter aspect collecting the 'report' output group.
+				// TODO: list of linter aspects should come from config
+				bazelCmd := bazelStartupArgs
+				bazelCmd = append(bazelStartupArgs, "build", "--aspects", "//:lint.bzl%eslint", "--output_groups=report")
+				bazelCmd = append(bazelCmd, QUIET_BZL_ARGS...)
+				bazelCmd = append(bazelCmd, args...)
+
+				if exitCode, err := bazel.WorkspaceFromWd.RunCommand(streams, nil, bazelCmd...); exitCode != 0 {
+					Log.Printf("Error running lint aspect: %v\n", err)
+
+					err = &aspecterrors.ExitError{
+						Err:      err,
+						ExitCode: exitCode,
+					}
+					return err
+				}
+
+				// Find the lint result files.
+				lintFiles, err := plugin.findLintResultFiles(streams, bazelStartupArgs)
+				if err != nil {
+					Log.Printf("Error collecting lint results: %v\n", err)
+
+					err = &aspecterrors.ExitError{
+						Err:      err,
+						ExitCode: 1,
+					}
+					return err
+				}
+
+				if len(lintFiles) == 0 {
+					return nil
+				}
+
+				// Output the lint results.
+				for _, f := range lintFiles {
+					lintResultBuf, err := os.ReadFile(f)
+					if err != nil {
+						Log.Printf("Error reading lint results file %q: %v\n", f, err)
+
+						err = &aspecterrors.ExitError{
+							Err:      err,
+							ExitCode: 1,
+						}
+						return err
+					}
+
+					lineResult := strings.TrimSpace(string(lintResultBuf))
+					if len(lineResult) > 0 {
+						fmt.Fprintln(streams.Stdout, lineResult)
+					}
+				}
+
+				return nil
+			},
+		),
+	}, nil
+}
+
+func (plugin *LintPlugin) findLintResultFiles(streams ioutils.Streams, bazelStartupArgs []string) ([]string, error) {
+	// TODO: use bazel query
+
+	var infoOutBuf bytes.Buffer
+	infoStreams := ioutils.Streams{
+		Stdin:  streams.Stdin,
+		Stdout: &infoOutBuf,
+		Stderr: streams.Stderr,
+	}
+
+	infoCmd := bazelStartupArgs
+	infoCmd = append(infoCmd, "info", "bazel-bin")
+	if exitCode, err := bazel.WorkspaceFromWd.RunCommand(infoStreams, nil, infoCmd...); exitCode != 0 {
+		return nil, err
+	}
+
+	binDir := strings.TrimSpace(infoOutBuf.String())
+
+	var findOutBuf bytes.Buffer
+	findCmd := exec.Command("find", binDir, "-type", "f", "-name", "*eslint-report.txt")
+	findCmd.Stdout = &findOutBuf
+	findCmd.Stderr = streams.Stderr
+	findCmd.Stdin = streams.Stdin
+
+	if err := findCmd.Run(); err != nil {
+		return nil, err
+	}
+
+	lintFiles := strings.TrimSpace(findOutBuf.String())
+	if len(lintFiles) == 0 {
+		return []string{}, nil
+	}
+
+	return strings.Split(lintFiles, "\n"), nil
+}
---
+++ release/release.bzl
@@ -0,0 +1,111 @@
+"""This module provides the macros for performing a release.
+"""
+
+load("@rules_go//go:def.bzl", "go_binary")
+load("@bazel_skylib//lib:dicts.bzl", "dicts")
+load("@bazel_skylib//rules:copy_file.bzl", "copy_file")
+load("@bazel_tools//tools/build_defs/hash:hash.bzl", "tools", _sha256 = "sha256")
+
+def _sha256_impl(ctx):
+    out = _sha256(ctx, ctx.file.artifact)
+    files = depset(direct = [out])
+    runfiles = ctx.runfiles(files = [out])
+    return [DefaultInfo(files = files, runfiles = runfiles)]
+
+sha256 = rule(
+    implementation = _sha256_impl,
+    attrs = dicts.add({
+        "artifact": attr.label(
+            allow_single_file = True,
+            mandatory = True,
+            doc = "The artifact whose sha256 value should be calculated.",
+        ),
+    }, tools),
+    doc = "Calculate the SHA256 hash value for a file.",
+    provides = [DefaultInfo],
+)
+
+def local_plugin(name, binary, path, **kwargs):
+    out = "_{}.out".format(name)
+    sum = "_{}.sum".format(name)
+
+    # Copy the default output of the binary rule.
+    # The path might be hard to predict, e.g. go_binary has an extra segment
+    copy_file(
+        name = out,
+        src = binary,
+        out = path,
+    )
+
+    # The plugin API requires a checksum for SecureConfig:
+    # https://github.com/hashicorp/go-plugin/pull/25
+    sha256(
+        name = sum,
+        artifact = out,
+    )
+
+    # Target you can build to do local dev
+    native.filegroup(
+        name = name,
+        srcs = [out, sum],
+        **kwargs
+    )
+
+PLATFORMS = [
+    struct(os = "darwin", arch = "amd64", ext = "", gc_linkopts = ["-s", "-w"]),
+    struct(os = "darwin", arch = "arm64", ext = "", gc_linkopts = ["-s", "-w"]),
+    struct(os = "linux", arch = "amd64", ext = "", gc_linkopts = ["-s", "-w"]),
+    struct(os = "linux", arch = "arm64", ext = "", gc_linkopts = ["-s", "-w"]),
+    struct(os = "windows", arch = "amd64", ext = ".exe", gc_linkopts = []),
+]
+
+def multi_platform_binaries(name, embed, prefix = ""):
+    """The multi_platform_binaries macro creates a go_binary for each platform.
+
+    Args:
+        name: the name of the filegroup containing all go_binary targets produced
+            by this macro.
+        embed: the list of targets passed to each go_binary target in this
+            macro.
+        prefix: an optional prefix added to the output Go binary file name.
+    """
+    targets = []
+    for platform in PLATFORMS:
+        target_name = "{}-{}-{}".format(name, platform.os, platform.arch)
+        go_binary(
+            name = target_name,
+            out = "{}{}-{}_{}{}".format(prefix, name, platform.os, platform.arch, platform.ext),
+            embed = embed,
+            gc_linkopts = platform.gc_linkopts,
+            goarch = platform.arch,
+            goos = platform.os,
+            pure = "on",
+            visibility = ["//visibility:public"],
+        )
+        targets.append(Label("//{}:{}".format(native.package_name(), target_name)))
+
+    native.filegroup(
+        name = name,
+        srcs = targets,
+    )
+
+def release(name, targets):
+    """The release macro creates the artifact copier script.
+
+    It's an executable script that copies all artifacts produced by the given
+    targets into the provided destination. See .github/workflows/release.yml.
+
+    Args:
+        name: the name of the genrule.
+        targets: a list of filegroups passed to the artifact copier.
+    """
+    native.genrule(
+        name = name,
+        srcs = targets,
+        outs = ["release.sh"],
+        executable = True,
+        cmd = "./$(location //release:create_release.sh) {locations} > \"$@\"".format(
+            locations = " ".join(["$(locations {})".format(target) for target in targets]),
+        ),
+        tools = ["//release:create_release.sh"],
+    )
