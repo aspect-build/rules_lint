2b0267b130f4da7e248a0d905058ee893b056c4c
Alex Eagle <alex@aspect.dev>
2023-10-11 16:39:25 -0700

    cleanup registry

--- example/lint.sh
+++ example/lint.sh
@@ -2,8 +2,13 @@
 # Shows an end-to-end workflow for linting without failing the build
 set -o errexit -o pipefail -o nounset
 
+if [ "$#" -eq 0 ]; then
+    echo "usage: lint.sh [target pattern...]"
+    exit 1
+fi
+
 # Produce report files
-bazel build //... --aspects //:lint.bzl%eslint,//:lint.bzl%buf -s --output_groups=report
+bazel build --aspects //:lint.bzl%eslint,//:lint.bzl%buf --output_groups=report $@
 
 # Process them
 find $(bazel info bazel-bin) -type f -name "*-report.txt" | xargs cat
