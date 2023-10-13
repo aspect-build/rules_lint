9afaca534296df155fa2401a1d186dc5ebb0639f
Alex Eagle <alex@aspect.dev>
2023-10-13 13:13:20 -0700

    chore: demonstrate PMD usage without rules_lint

---
+++ example/src/Foo.java
@@ -0,0 +1,10 @@
+// Violation of https://docs.pmd-code.org/latest/pmd_rules_java_errorprone.html#finalizeoverloaded
+// Demo of detection:
+// bazel run :pmd --run_under="cd $PWD &&" -- -d src -R "category/java/errorprone.xml/FinalizeOverloaded" 
+// src/equals_null.java:6: FinalizeOverloaded:     Finalize methods should not be overloaded
+// Error: bazel exited with exit code: 4
+public class Foo {
+    // this is confusing and probably a bug
+    protected void finalize(int a) {
+    }
+}
