aspect-build/rules_lint

commit 6ea66fb4c7b653f8e061d0997bfb74f965609835

fix: make Markdown a separate 'language'
It's pretty confusing that it's lumped under 'JavaScript' currently.

Fixes #81

format/private/format.sh
@@ -85,8 +85,19 @@ else
 fi
 bin=$(rlocation {{buildifier}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running Buildifier..."
+  echo "Formatting Starlark with Buildifier..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin -mode="$mode"
+fi
+
+if [ "$#" -eq 0 ]; then
+  files=$(git ls-files --cached --modified --other --exclude-standard '*.md' | { grep -vE "^$(git ls-files --deleted)$" || true; })
+else
+  files=$(find "$@" -name '*.md')
+fi
+bin=$(rlocation {{prettier}})
+if [ -n "$files" ] && [ -n "$bin" ]; then
+  echo "Formatting Markdown with Prettier..."
+  echo "$files" | tr \\n \\0 | xargs -0 $bin $prettiermode
 fi
 
 if [ "$#" -eq 0 ]; then
@@ -96,7 +107,7 @@ else
 fi
 bin=$(rlocation {{prettier}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running Prettier..."
+  echo "Formatting JavaScript with Prettier..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $prettiermode
 fi
 
@@ -107,7 +118,7 @@ else
 fi
 bin=$(rlocation {{prettier-sql}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running Prettier (sql)..."
+  echo "Running SQL with Prettier..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $prettiermode
 fi
 
@@ -118,7 +129,7 @@ else
 fi
 bin=$(rlocation {{ruff}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running ruff..."
+  echo "Formatting Python with ruff..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $ruffmode
 fi
 
@@ -129,7 +140,7 @@ else
 fi
 bin=$(rlocation {{terraform}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running terraform..."
+  echo "Formatting terraform..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin fmt $tfmode
 fi
 
@@ -140,7 +151,7 @@ else
 fi
 bin=$(rlocation {{jsonnetfmt}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running jsonnetfmt..."
+  echo "Formatting Jsonnet with jsonnetfmt..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $jsonnetmode
 fi
 
@@ -151,7 +162,7 @@ else
 fi
 bin=$(rlocation {{java-format}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running java-format..."
+  echo "Formatting Java with java-format..."
   # Setting JAVA_RUNFILES to work around https://github.com/bazelbuild/bazel/issues/12348
   echo "$files" | tr \\n \\0 | JAVA_RUNFILES="${RUNFILES_MANIFEST_FILE%_manifest}" xargs -0 $bin $javamode
 fi
@@ -163,7 +174,7 @@ else
 fi
 bin=$(rlocation {{ktfmt}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running ktfmt..."
+  echo "Formatting Kotlin with ktfmt..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $ktmode
 fi
 
@@ -174,7 +185,7 @@ else
 fi
 bin=$(rlocation {{scalafmt}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running scalafmt..."
+  echo "Formatting Scala with scalafmt..."
   # Setting JAVA_RUNFILES to work around https://github.com/bazelbuild/bazel/issues/12348
   echo "$files" | tr \\n \\0 | JAVA_RUNFILES="${RUNFILES_MANIFEST_FILE%_manifest}" xargs -0 $bin $scalamode
 fi
@@ -186,7 +197,7 @@ else
 fi
 bin=$(rlocation {{gofmt}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running gofmt..."
+  echo "Formatting Go with gofmt..."
   # gofmt doesn't produce non-zero exit code so we must check for non-empty output
   # https://github.com/golang/go/issues/24230
   if [ "$mode" == "check" ]; then
@@ -208,7 +219,7 @@ else
 fi
 bin=$(rlocation {{shfmt}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running shfmt..."
+  echo "Formatting Bash/Shell with shfmt..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $shfmtmode
 fi
 
@@ -232,7 +243,7 @@ fi
 bin=$(rlocation {{buf}})
 
 if [ -n "$files" ] && [ -n "$bin" ]; then
-  echo "Running buf..."
+  echo "Formatting Protobuf with buf..."
   for file in $files; do
     $bin $bufmode $file
   done

format/private/formatter_binary.bzl
@@ -5,6 +5,7 @@ load("@aspect_bazel_lib//lib:paths.bzl", "to_rlocation_path")
 # Per the formatter design, each language can only have a single formatter binary
 _TOOLS = {
     "javascript": "prettier",
+    "markdown": "prettier",
     "python": "ruff",
     "starlark": "buildifier",
     "jsonnet": "jsonnetfmt",
@@ -62,7 +63,7 @@ def _formatter_binary_impl(ctx):
 formatter_binary_lib = struct(
     implementation = _formatter_binary_impl,
     attrs = dict({
-        k: attr.label(doc = "a binary target that runs {}".format(v), executable = True, cfg = "exec", allow_files = True)
+        k: attr.label(doc = "a binary target that runs {} (or another tool with compatible CLI arguments)".format(v), executable = True, cfg = "exec", allow_files = True)
         for k, v in _TOOLS.items()
     }, **{
         "_bin": attr.label(default = "//format/private:format.sh", allow_single_file = True),

example/tools/BUILD
@@ -122,6 +122,7 @@ multi_formatter_binary(
     javascript = ":prettier",
     jsonnet = ":jsonnetfmt",
     kotlin = ":ktfmt",
+    markdown = ":prettier",
     protobuf = ":buf",
     python = ":ruff",
     scala = ":scalafmt",

commit 906c4b40f0bff5643864c3252cfa73d8d1af7a95

fix(formatter): markdown needs a distinct tool entry
Otherwise it overloads the one for JavaScript: we reverse the _TOOLS map and don't find an entry for the first attribute we look for, and have no prettier binary substituted into the script.

I broke this in https://github.com/aspect-build/rules_lint/commit/6ea66fb4c7b653f8e061d0997bfb74f965609835

Note, there ought to be more testing that would have caught this. I
spent some time writing a bats test here - but it doesn't catch the
problem in our current example since it always configures the formatter
for all languages. We would need a test fixture that creates a bunch of
different formatter binaries.

Will put that off until we rewrite the formatter binary.

format/private/format.sh
@@ -94,7 +94,7 @@ if [ "$#" -eq 0 ]; then
 else
   files=$(find "$@" -name '*.md')
 fi
-bin=$(rlocation {{prettier}})
+bin=$(rlocation {{prettier-md}})
 if [ -n "$files" ] && [ -n "$bin" ]; then
   echo "Formatting Markdown with Prettier..."
   echo "$files" | tr \\n \\0 | xargs -0 $bin $prettiermode

format/private/formatter_binary.bzl
@@ -5,7 +5,7 @@ load("@aspect_bazel_lib//lib:paths.bzl", "to_rlocation_path")
 # Per the formatter design, each language can only have a single formatter binary
 _TOOLS = {
     "javascript": "prettier",
-    "markdown": "prettier",
+    "markdown": "prettier-md",
     "python": "ruff",
     "starlark": "buildifier",
     "jsonnet": "jsonnetfmt",