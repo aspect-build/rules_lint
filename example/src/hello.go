6c633efba73d6a45be4dd7cd73e40b05a681bed9
Sean R. Abraham <sabraham@videoamp.com>
2024-02-09 13:05:23 -0700

    chore: demonstrate golangci-lint not working with deps
    I get an error like below when I run
    bazel lint src:hello_go
    as a result of the go_binary depending on another go target
    
    this is a demo of https://github.com/aspect-build/rules_lint/issues/129
    
    INFO: Analyzed target //src:hello_go (0 packages loaded, 0 targets configured).
    INFO: From golangcilint src/golangcilint.hello_go.aspect_rules_lint.report:
    level=warning msg="[runner] Can't run linter goanalysis_metalinter: buildir: failed to load package : could not load export data: no export data for \"gopher\""
    level=error msg="Running error: 1 error occurred:\n\t* can't run linter goanalysis_metalinter: buildir: failed to load package : could not load export data: no export data for \"gopher\"\n\n"

--- example/src/hello.go
+++ example/src/hello.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"gopher"
+	"log"
 )
 
-const (
-	w = "world"
-)
+// staticcheck won't like this
+var notUsed string
 
 func main() {
-	hello := fmt.Sprintf("Hello %s\n", w)
+	s := []string{"a"}
+	// staticcheck also won't like this
+	if s != nil {
+		for _, v := range s {
+			log.Println(v)
+		}
+	}
+	hello := fmt.Sprintf("Hello %s\n", gopher.Name())
 	fmt.Printf(hello)
+	_ = s
 }
