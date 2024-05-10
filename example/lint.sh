9668bf46006cf2d18b6fa855fc31074a1347dd16
Alex Eagle <alex@aspect.dev>
2024-05-10 07:01:53 -0700

    fix: lint.sh pass norun_validations (#231)
    * fix: lint.sh pass norun_validations
    
    See https://github.com/aspect-build/rules_ts/pull/574#issuecomment-2073632879
    
    * Update lint.sh

--- example/lint.sh
+++ example/lint.sh
@@ -25,6 +25,9 @@ filter='.namedSetOfFiles | values | .files[] | ((.pathPrefix | join("/")) + "/" 
 # NB: perhaps --remote_download_toplevel is needed as well with remote execution?
 args=(
 	"--aspects=$(echo //tools/lint:linters.bzl%{buf,eslint,flake8,ktlint,pmd,ruff,shellcheck,vale} | tr ' ' ',')"
+	# Allow lints of code that fails some validation check
+        # See https://github.com/aspect-build/rules_ts/pull/574#issuecomment-2073632879
+        "--norun_validations"
 	"--build_event_json_file=$buildevents"
 )
 report_args=(
