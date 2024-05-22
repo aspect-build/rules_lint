a5db77ed443ef9a0d6423514ceda234f8ff26376
Alex Eagle <alex@aspect.dev>
2024-05-22 09:51:27 -0700

    fix: stylistic TS checks also type-aware
    Following https://typescript-eslint.io/getting-started/typed-linting#shared-configurations

--- example/eslint.config.mjs
+++ example/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylistic,
+  ...tseslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
       parserOptions: {
