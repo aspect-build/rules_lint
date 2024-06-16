93c86c4e04be23c2c7bfec256c714c3c674a2142
Chris Brown <chris.c.brown@siemens.com>
2024-06-16 00:57:25 -0400

    fix: remove workaround_windows.bat

--- example/workaround_windows.bat
+++
@@ -1,5 +0,0 @@
-:: aspect-cli doesn't support windows, so disable it
-del %~dp0.bazeliskrc
-:: ../.bazelversion syntax not supported by windows bazelisk
-del %~dp0.bazelversion
-copy %~dp0\..\.bazelversion %~dp0
