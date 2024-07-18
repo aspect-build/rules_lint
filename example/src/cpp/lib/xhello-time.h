3391a1bbf6559433b9ed2774aa3602f3595a4946
Alex Eagle <alex@aspect.dev>
2024-07-17 18:52:14 -0700

    chore: run formatter (#336)

--- example/src/cpp/lib/xhello-time.h
+++ example/src/cpp/lib/xhello-time.h
@@ -1,13 +1,14 @@
 #ifndef LIB_HELLO_TIME2_H_
 #define LIB_HELLO_TIME2_H_
 
-#include <string>
 #include <stdio.h>
 
+#include <string>
+
 inline void print_localtime3() {
-    std::string a = "time";
-    for (int i=0; i<a.size(); ++i) {
-        printf("%s", a.c_str()[i]);
-    }
+  std::string a = "time";
+  for (int i = 0; i < a.size(); ++i) {
+    printf("%s", a.c_str()[i]);
+  }
 }
 #endif
