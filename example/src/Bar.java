069ab7dbc658cb96fefd387fed4b182a499d1c53
Alex Eagle <alex@aspect.dev>
2024-09-17 13:31:09 -0700

    Update Bar.java

--- example/src/Bar.java
+++ example/src/Bar.java
@@ -1,6 +1,8 @@
 package src;
 
 // Unused imports are suppressed in suppressions.xml, so this should not raise issue.
+import java.util.Objects;
+import java.io.BufferedInputStream;
 
 public class Bar {
   // Max line length set to 20, so this should raise issue.
