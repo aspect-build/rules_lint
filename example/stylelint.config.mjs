533ca1c6206d5c6a4163d7e6c819253967a22c8d
Connor McEntee <sallustfire@gmail.com>
2024-09-20 12:47:58 -0600

    fix: include stylelint config deps (#394)
    If the stylelint config is declared via js_library and has a dependency on
    other sources or npm packages, these dependencies are now included in the
    linting action.

--- example/stylelint.config.mjs
+++ example/stylelint.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('stylelint').Config} */
 export default {
+  extends: 'stylelint-config-standard',
   rules: {
     "block-no-empty": true,
     "declaration-block-no-duplicate-properties": true,
