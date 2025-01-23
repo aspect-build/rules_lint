db66dece03bcdd78d619831f52a48bdc16f50415
Alex Eagle <alex@aspect.dev>
2025-01-23 11:17:54 -0800

    fix: release snippet missing escaping
    .github/workflows/release_prep.sh: line 14: WORKSPACE.bazel: command not found

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -27,7 +27,7 @@ bazel_dep(name = "aspect_rules_lint", version = "${TAG:1}")
 
 ## Using WORKSPACE
 
-Paste this snippet into your `WORKSPACE.bazel` file:
+Paste this snippet into your \`WORKSPACE.bazel\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
