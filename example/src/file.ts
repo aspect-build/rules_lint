ed1207aa16463939ab363aec2326ceab29a9963f
Alex Eagle <alex@aspect.dev>
2025-05-07 15:49:17 -0700

    Update file.ts

--- example/src/file.ts
+++ example/src/file.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { Greeter } from "./file-dep";
 
 // this is a linting violation, and is auto-fixed under `--fix`
-const a: string = "a";
+const a: string = "ab";
 console.log(a);
 
 // linting violation; not auto-fixable
