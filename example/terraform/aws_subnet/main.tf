19da28ffed36ba2549284fa0f12574e1ad41733f
Spencer Connaughton <spencer@spencerconnaughton.com>
2025-10-14 11:37:51 -0400

    Incorrectly format main.tf.

--- example/terraform/aws_subnet/main.tf
+++ example/terraform/aws_subnet/main.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "example" {
-  vpc_id            = var.vpc_id
+  vpc_id      = var.vpc_id
   cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-2a"
+     availability_zone = "us-east-2a"
 
-  tags = {
+  tags={
     Name = "aspect-rules-lint-example"
   }
 }
