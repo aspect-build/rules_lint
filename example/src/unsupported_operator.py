b5aec67998cfd17c15f0c9527ca93f6ef6fc2430
Dan Herrera <whoahbot@gmail.com>
2025-12-03 01:28:37 +0000

    Address PR comments
    - Removed ty mirroring in favor of multitool usage
    - Fixed running example ty linting with a standalone command.
    - Fixed a closing paren in the docs.
    - Dropped `fetch_ty` helper.

--- example/src/unsupported_operator.py
+++ example/src/unsupported_operator.py
@@ -1,3 +1,3 @@
 # Demo with just running ty from the examples dir:
-# $ ./lint.sh src:unsupported_operator
+# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check --config-file=ty.toml src/*.py
 a = 10 + "test"
--- example/test/unsupported_operator.py
+++ example/test/unsupported_operator.py
@@ -1,3 +1,3 @@
 # Demo with just running ty:
-# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check --config=ty.toml src/*.py
+# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check --config-file=ty.toml test/*.py
 a = 10 + "test"
