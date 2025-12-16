840bf2777d6fbb97957819f16c6a17a776b97d38
Alex Eagle <alex@aspect.dev>
2025-12-16 14:08:06 -0800

    simplify

--- example/test/unsupported_operator.py
+++ example/test/unsupported_operator.py
@@ -1,3 +1,3 @@
 # Demo with just running ty:
-# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check --config-file=ty.toml test/*.py
+# $ bazel run --run_under="cd $PWD &&" -- @aspect_rules_lint//lint:ty_bin check --config-file=ty.toml test/*.py
 a = 10 + "test"
