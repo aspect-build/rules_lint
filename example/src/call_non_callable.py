4d48abda2938aba82e951afc82a47620f8be911c
Alex Eagle <alex@aspect.dev>
2025-12-16 13:57:09 -0800

    update ruff and ty

--- example/src/call_non_callable.py
+++ example/src/call_non_callable.py
@@ -1,6 +1,5 @@
 # Demo with just running ty:
-# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check \
-#     --config-file=ty.toml src/call_non_callable.py
+# $ bazel run --run_under="cd $PWD &&" -- //tools/lint:ty check --config-file=ty.toml src/call_non_callable.py
 B = 1
-# This error should be ignored, as it is specified as ignored in src/ty.toml
+# This error should be ignored, as it is specified as ignored in ty.toml
 B()
