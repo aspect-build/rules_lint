2fcf4db75f5c9cca2ad5254ccef29320f92827a2
Alex Eagle <alex@aspect.dev>
2025-12-31 14:35:55 -0800

    repro: ruamel.yaml causes an error with ty:
    jq: error (at bazel-out/darwin_arm64-fastbuild/bin/src/unused_import_pyi.AspectRulesLintRuff.report:13): Cannot iterate over null (null)

--- examples/python/app/__main__.py
+++ examples/python/app/__main__.py
@@ -1,2 +1,6 @@
 import requests
+from ruamel.yaml import YAML
+
+yaml=YAML(typ='safe')   # default, if not specified, is 'rt' (round-trip)
+yaml.load(doc=open("test.yaml"))
 print(requests.get("https://www.google.com/generate_204").status_code)
