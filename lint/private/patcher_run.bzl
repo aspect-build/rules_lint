ab30b8191e35dde07dc8f1b68fd0e269372792c6
Borja Lorente <blorente.me@gmail.com>
2026-01-11 16:00:39 +0000

    feat(clippy): Implement --fix flag for clippy lints
    # Conflicts:
    #	examples/rust/src/BUILD
    #	lint/clippy.bzl

---
+++ lint/private/patcher_run.bzl
@@ -0,0 +1,64 @@
+load("@bazel_lib//lib:expand_make_vars.bzl", _expand_locations = "expand_locations")
+load("//lint/private:patcher_action.bzl", "patcher_attrs", _run_patcher = "run_patcher")
+
+def _patcher_run_impl(ctx):
+    diff_file = ctx.actions.declare_file("_{}.diff".format(ctx.label.name))
+
+    files_to_diff = ctx.files.files_to_diff
+    data = ctx.files.data
+    env = ctx.attr.env
+    bindir = "."
+
+    inputs = [
+        ctx.executable.tool,
+    ] + files_to_diff + data
+
+    tool_args = [
+        _expand_locations(ctx, arg, ctx.attr.data)
+        for arg in ctx.attr.tool_args
+    ]
+
+    _run_patcher(
+        ctx,
+        ctx.executable,
+        inputs = inputs,
+        args = tool_args,
+        files_to_diff = [f.path for f in files_to_diff],
+        patch_out = diff_file,
+        tools = [ctx.executable.tool],
+        patch_cfg_env = dict(env, **{"BAZEL_BINDIR": bindir}),
+        env = env,
+        mnemonic = ctx.attr.mnemonic,
+        progress_message = "Running patcher in %{label}",  # TODO BL: This could be better.
+    )
+
+    return [DefaultInfo(files = depset([diff_file]))]
+
+patcher_run = rule(
+    doc = "A rule that wraps patcher_action to make running tests easier.",
+    implementation = _patcher_run_impl,
+    attrs = patcher_attrs | {
+        "tool": attr.label(
+            mandatory = True,
+            executable = True,
+            cfg = "exec",
+        ),
+        "files_to_diff": attr.label_list(
+            mandatory = True,
+            allow_files = True,
+        ),
+        "mnemonic": attr.string(
+            default = "Patcher",
+        ),
+        "tool_args": attr.string_list(
+            default = [],
+        ),
+        "data": attr.label_list(
+            default = [],
+            allow_files = True,
+        ),
+        "env": attr.string_dict(
+            default = {},
+        ),
+    },
+)
--- lint/rust/rust.cli.js
+++ lint/rust/rust.cli.js
@@ -2,40 +2,108 @@
 
 const fs = require("fs");
 const {
-  diagnosticsToSarifPatchFile,
+  diagnosticsToSarif,
+  applyDiagnosticsAsPatches,
   diagnosticsToHumanReadable,
 } = require("./rust.diagnostic-formatter");
+
+/**
+ * @typedef {"human-readable" | "patch" | "sarif"} CliCommand
+ *
+ * @typedef CliOptions
+ * @property {CliCommand} command - Command to run.
+ * @property {string} inputFile - Path to the input file, relative to where the binary is being run.
+ * @property {string | null} outputFile - Path to the input file, relative to where the binary is being run.
+ */
+
+/**
+ * @param {CliCommand | null} command
+ */
+function printUsage(command) {
+  switch (command) {
+    case "human-readable":
+      console.error(
+          "Usage: node rust.cli.js human-readable <rustc-diagnostic-json-file> <output-file>"
+      );
+      break;
+    case "sarif":
+      console.error(
+          "Usage: node rust.cli.js sarif <rustc-diagnostic-json-file> <output-file>"
+      );
+      break;
+    case "patch":
+      console.error(
+          "Usage: node rust.cli.js patch <rustc-diagnostic-json-file>"
+      );
+      break;
+    case null:
+      console.error(
+          "Usage: node rust.cli.js <command> [<arg>...]"
+      );
+      console.error('  command: "human-readable", "patch", or "sarif"');
+      break;
+  }
+}
+
+/**
+ * @return {CliOptions}
+ */
+function parseOptions() {
+  const argv = process.argv;
+
+  if (argv.length < 4) {
+    printUsage(null);
+    process.exit(1);
+  }
+
+  const command = argv[2];
+
+  // Validate command
+  if (command !== "human-readable" && command !== "sarif" && command !== "patch") {
+    console.error(
+        'Error: unrecognized command'
+    );
+    printUsage(null);
+    process.exit(1);
+  }
+
+  const inputFile = argv[3];
+
+  if (!inputFile) {
+    printUsage(command)
+    process.exit(1);
+  }
+
+  let outputFile = null;
+  if (shouldWriteOutput(command)) {
+    if (argv.length < 5) {
+      printUsage(command);
+      process.exit(1);
+    }
+    outputFile = argv[4];
+  }
+
+  return {
+    command,
+    inputFile,
+    outputFile,
+  }
+}
 
 /**
  * Main function that processes command line arguments and executes the appropriate function
  */
 function main() {
-  // Check if we have the correct number of arguments
-  if (process.argv.length !== 5) {
-    console.error(
-      "Usage: node rust.cli.js <command> <input-file> <output-file>"
-    );
-    console.error('  command: "human-readable" or "sarif"');
-    process.exit(1);
-  }
+  const {
+    command, inputFile, outputFile
+  } = parseOptions();
 
-  const outputType = process.argv[2];
-  const inputFile = process.argv[3];
-  const outputFile = process.argv[4];
-
-  // Validate output type
-  if (outputType !== "human-readable" && outputType !== "sarif") {
-    console.error(
-      'Error: output-type must be either "human-readable" or "sarif"'
-    );
-    process.exit(1);
-  }
-
+  let diagnostics = [];
   try {
     // Read and parse the input file
     const fileContent = fs.readFileSync(inputFile, "utf8");
 
-    const diagnostics = fileContent
+    diagnostics = fileContent
       .split("\n")
       .filter((line) => line.trim() !== "")
       .filter((line) => line.startsWith("{"))
@@ -45,28 +113,53 @@ function main() {
         } catch (err) {
           console.error(`Error parsing JSON line: ${line}`);
           console.error(err);
-          return null;
+          throw err;
         }
       })
       .filter((item) => item !== null);
+  } catch (err) {
+    console.error(`Error gathering diagnostics files: ${err.message}`);
+    process.exit(1);
+  }
 
-    // Process the diagnostics based on output type
-    let outputContent;
-    if (outputType === "human-readable") {
+  // Process the diagnostics based on output type
+  let outputContent;
+  try {
+    if (command === "human-readable") {
       outputContent = diagnosticsToHumanReadable(diagnostics);
+    } else if (command === "patch") {
+      outputContent = applyDiagnosticsAsPatches(diagnostics);
     } else {
       // outputType is 'sarif'
-      const sarif = diagnosticsToSarifPatchFile(diagnostics);
+      const sarif = diagnosticsToSarif(diagnostics);
       outputContent = JSON.stringify(sarif, null, 2);
     }
-
-    // Write the output
-    fs.writeFileSync(outputFile, outputContent);
-    console.log(`Successfully wrote output to ${outputFile}`);
   } catch (err) {
     console.error(`Error processing files: ${err.message}`);
     process.exit(1);
   }
+
+  if (!shouldWriteOutput(command)) {
+    console.log(`Command '${command}' requires no output. We're done!`)
+    process.exit(0);
+  }
+
+  try {
+    // Write the output
+    fs.writeFileSync(outputFile, outputContent);
+    console.log(`Successfully wrote output to ${outputFile}`);
+  } catch (err) {
+    console.error(`Error writing outputs: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+/**
+ * @param {CliCommand} command - The command that was passed to the cli.
+ * @return {boolean} - True if the output should be written to disk.
+ */
+function shouldWriteOutput(command) {
+  return command === "human-readable" || command === "sarif";
 }
 
 // Execute the main function
