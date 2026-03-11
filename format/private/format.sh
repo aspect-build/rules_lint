a9a7d2fa7ad574b365e24ceab1fb890ca46a1117
Matt Brown <mattbrown@spotify.com>
2026-03-11 14:40:00 -0400

    format: remove .jsh and .jav extensions for java
    google-java-format will only format files that end in `.java`, printing a warning for anything else: 
    
    https://github.com/google/google-java-format/blob/2f7e558e586788110dfda603652f68915d890cb0/core/src/main/java/com/google/googlejavaformat/java/Main.java#L142-L145

--- format/private/format.sh
+++ format/private/format.sh
@@ -128,7 +128,7 @@ function ls-files {
       'JSON') patterns=('.all-contributorsrc' '.arcconfig' '.auto-changelog' '.c8rc' '.htmlhintrc' '.imgbotconfig' '.nycrc' '.tern-config' '.tern-project' '.watchmanconfig' 'Pipfile.lock' 'composer.lock' 'deno.lock' 'flake.lock' 'mcmod.info' '*.json' '*.4DForm' '*.4DProject' '*.avsc' '*.geojson' '*.gltf' '*.har' '*.ice' '*.JSON-tmLanguage' '*.jsonl' '*.mcmeta' '*.tfstate' '*.tfstate.backup' '*.topojson' '*.webapp' '*.webmanifest' '*.yy' '*.yyp') ;;
       'JSON with Comments') patterns=( '*.jsonc' '*.code-snippets' '*.code-workspace' '*.sublime-build' '*.sublime-commands' '*.sublime-completions' '*.sublime-keymap' '*.sublime-macro' '*.sublime-menu' '*.sublime-mousemap' '*.sublime-project' '*.sublime-settings' '*.sublime-theme' '*.sublime-workspace' '*.sublime_metrics' '*.sublime_session' '.babelrc' '.devcontainer.json' '.eslintrc.json' '.jscsrc' '.jshintrc' '.jslintrc' '.swcrc' 'api-extractor.json' 'devcontainer.json' 'jsconfig.json' 'language-configuration.json' 'tsconfig.json' 'tslint.json') ;;
       'JSON5') patterns=('*.json5') ;;
-      'Java') patterns=('*.java' '*.jav' '*.jsh') ;;
+      'Java') patterns=('*.java') ;;
       'JavaScript') patterns=('Jakefile' '*.js' '*._js' '*.bones' '*.cjs' '*.es' '*.es6' '*.frag' '*.gs' '*.jake' '*.javascript' '*.jsb' '*.jscad' '*.jsfl' '*.jslib' '*.jsm' '*.jspre' '*.jss' '*.jsx' '*.mjs' '*.njs' '*.pac' '*.sjs' '*.ssjs' '*.xsjs' '*.xsjslib') ;;
       'HTML Jinja') patterns=('*.html.jinja' '*.html.j2' '*.html.jinja2') ;;
       'Jsonnet') patterns=('*.jsonnet' '*.libsonnet') ;;
